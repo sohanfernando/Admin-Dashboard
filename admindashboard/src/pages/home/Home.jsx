c87d19d3329692d702c2e18fca5a25382742a947
Sohan Fernando <sohanfernando7@gmail.com>
2024-10-15 19:12:11 +0530

    Code Updated

--- admindashboard/src/pages/home/Home.jsx
+++ admindashboard/src/pages/home/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import "./home.scss"
+import Sidebar from '../../components/sidebar/Sidebar';
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <Sidebar />
+      <div className="homeContainer">container</div>
+    </div>
   )
 }
+
+export default Home;
