eb606fa7b3fdb917ffbc453bf7b20b051474bdf0
Sohan Fernando <sohanfernando7@gmail.com>
2024-10-18 17:57:12 +0530

    Update Home.jsx, Widget.jsx

--- admindashboard/src/pages/home/Home.jsx
+++ admindashboard/src/pages/home/Home.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import "./home.scss"
 import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
+import Widget from '../../components/widget/Widget';
 
 export const Home = () => {
   return (
     <div className='home'>
       <Sidebar />
-      <div className="homeContainer">container</div>
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          
+        </div>
+      </div>
     </div>
   )
 }
