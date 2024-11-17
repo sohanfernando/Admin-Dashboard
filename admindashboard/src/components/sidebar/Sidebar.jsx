24e1b2db6f45553b10387caf7cc6935abbae1c72
Sohan Fernando <sohanfernando7@gmail.com>
2024-11-18 01:19:05 +0530

    dark mode is complete

--- admindashboard/src/components/sidebar/Sidebar.jsx
+++ admindashboard/src/components/sidebar/Sidebar.jsx
@@ -12,12 +12,17 @@ import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
 import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
-
+import {Link} from "react-router-dom";
+import { DarkModeContext } from '../../context/darkModeContext';
+import { useContext } from 'react';
 const Sidebar = () => {
+    const { dispatch } = useContext(DarkModeContext)
   return (
     <div className='sidebar'>
         <div className="top">
-            <span className='logo'>admin dashboard</span>
+            <Link to="/" style={{textDecoration: "none"}}>
+                <span className='logo'>admin dashboard</span>
+            </Link>
         </div>
         <hr />
         <div className="center">
@@ -28,14 +33,18 @@ const Sidebar = () => {
                     <span>Dashboard</span>
                 </li>
                 <p className='title'>LISTS</p>
-                <li>
-                    <PersonOutlineOutlinedIcon className='icon' />
-                    <span>Users</span>
-                </li>
-                <li>
-                    <StoreIcon className='icon' />
-                    <span>Products</span>
-                </li>
+                <Link to="/users" style={{textDecoration: "none"}}>
+                    <li>
+                        <PersonOutlineOutlinedIcon className='icon' />
+                        <span>Users</span>
+                    </li>
+                </Link>
+                <Link to="/products" style={{textDecoration: "none"}}>
+                    <li>
+                        <StoreIcon className='icon' />
+                        <span>Products</span>
+                    </li>
+                </Link>
                 <li>
                     <CreditCardIcon className='icon' />
                     <span>Orders</span>
@@ -80,9 +89,11 @@ const Sidebar = () => {
         <div className="bottom">
             <div
                 className='colorOption'
+                onClick={() => dispatch({ type: "LIGHT"})}
             ></div>
             <div
                 className='colorOption'
+                onClick={() => dispatch({ type: "DARK"})}
             ></div>
         </div>
     </div>
