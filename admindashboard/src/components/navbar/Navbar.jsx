26269595d1804bb1b8d1b0f8e1589e9fc437340b
Sohan Fernando <sohanfernando7@gmail.com>
2024-11-18 01:49:56 +0530

    Update Navbar.jsx

--- admindashboard/src/components/navbar/Navbar.jsx
+++ admindashboard/src/components/navbar/Navbar.jsx
@@ -7,8 +7,12 @@ import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlin
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
 import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
+import { DarkModeContext } from "../../context/darkModeContext";
+import { useContext } from "react";
 
 const Navbar = () => {
+  const { dispatch } = useContext(DarkModeContext);
+
   return (
     <div className='navbar'>
       <div className="wrapper">
@@ -24,6 +28,7 @@ const Navbar = () => {
           <div className="item">
             <DarkModeOutlinedIcon
               className='icon'
+              onClick={() => dispatch({ type: "TOGGLE"})}
             />
           </div>
           <div className="item">
